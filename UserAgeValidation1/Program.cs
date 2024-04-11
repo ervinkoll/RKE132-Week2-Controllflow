f7209a7940285cabaadfc787b799f9a35f0a307b
Ervin <Ervin.koll@tktk.ee>
2024-04-11 13:31:37 +0300

    add week2 code

---
+++ UserAgeValidation1/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//rakednus küsib kasutajal sisestada tema vanuse
+//kui kasutaja vanus on väiksem kui 13 konsoolis kuvatakse:
+//"You are too young to use Instagram"
+//konsoolis kuvatakse "Welcome to Instagram"
+
+Console.WriteLine("Enter your age");
+
+string userAge = Console.ReadLine();
+
+int userAgeNum = 0;
+
+//boolean - true/false
+
+bool isAgeNumber = Int32.TryParse(userAge, out userAgeNum);
+
+//int userAge = Int32.Parse(Console.ReadLine());//"13" - heap, 13 - stack
+
+Console.WriteLine($"Parse result {isAgeNumber}. User is {userAgeNum} years old.");
+
+if(isAgeNumber)
+{
+    if(userAgeNum >= 13)
+    {
+        Console.WriteLine("Welcome to Instagram");
+    }
+    else
+    {
+        Console.WriteLine("You are too young to use Instagram,");
+    }
+}
+else
+{
+    Console.WriteLine("Could not read your age.");
+}
---
+++ Week2/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+//uued andmete tüübid: int ja char
+//if-statements (control flow)
+
